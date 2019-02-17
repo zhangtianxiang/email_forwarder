29f6a576fa2906f6f65df11979e1e9b78113de83
ZhangTianxiang <ztx97@qq.com>
2019-02-17 17:35:28 +0800

    Fix bugs

--- forwarder.py
+++ forwarder.py
@@ -8,14 +8,25 @@ import _thread
 from flask import Flask, jsonify, request, make_response
 from flask_mail import Mail, Message
 
+
+RECEIVERS = ['ztx97@qq.com']
+FORWARDER_PASS = ''
+DEBUG = False
+HOST = '127.0.0.1'
+PORT = 5050
+
+
 app = Flask(__name__)
+
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'tianxiang971016@gmail.com'
-app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_PASSWORD'] = FORWARDER_PASS
+
 mail = Mail(app)
+
 
 def send_email_sync(title='Empty Title',sender='tianxiang971016@gmail.com',receivers=['ztx97@qq.com'],content='Empty content'):
     print('[msg_sender sending]')
@@ -26,17 +37,23 @@ def send_email_sync(title='Empty Title',sender='tianxiang971016@gmail.com',recei
         mail.send(msg)
     print('[msg_sender sent]')
 
+
 def send_email_async(title='Empty Title',sender='tianxiang971016@gmail.com',receivers=['ztx97@qq.com'],content='Empty content'):
     _thread.start_new_thread(send_email_sync,(title,sender,receivers,content))
+
+
+def allow_cross_response(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'x-requested-with,Content-Type'
+    response.headers['Access-Control-Max-Age'] = '3600'
+    return response
+
 
 @app.route('/')
 def index():
     return 'ztx email forwarder running'
 
-# @app.route('/api/test_send_email',methods=['GET','POST'])
-# def test_send_email():
-#     send_email_async(title='祝你生日快乐！',sender='admin@forwarder.com',receivers=['ztx97@qq.com'],content='祝你生日快乐哦！')
-#     return jsonify({'state':0,'message':'sent'})
 
 @app.route('/api/send_email_async',methods=['POST'])
 def api_send_email_async():
@@ -44,7 +61,7 @@ def api_send_email_async():
     replyemail = request.form.get('replyemail')
     message = request.form.get('message')
     if not name:
-        return jsonify({'state':0,'message':'Empty name'})
+        return allow_cross_response(make_response(jsonify({'state':1,'message':'Empty name'})))
     if not message:
         message = ''
     else:
@@ -52,13 +69,8 @@ def api_send_email_async():
     message += 'From '+name
     if replyemail:
         message += '('+replyemail+')'
-    send_email_async(title='祝你生日快乐！',sender='admin@forwarder.com',receivers=['ztx97@qq.com'],content=message)
-    response = make_response(jsonify({'state':0,'message':'sent'}))
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'x-requested-with,Content-Type'
-    response.headers['Access-Control-Max-Age'] = '3600';
-    return response
+    send_email_async(title='祝你生日快乐！',sender='admin@forwarder.com',receivers=RECEIVERS,content=message)
+    return allow_cross_response(make_response(jsonify({'state':0,'message':'sent'})))
 
 
 @app.route('/api/send_email_sync',methods=['POST'])
@@ -67,7 +79,7 @@ def api_send_email_sync():
     replyemail = request.form.get('replyemail')
     message = request.form.get('message')
     if not name:
-        return jsonify({'state':0,'message':'Empty name'})
+        return allow_cross_response(make_response(jsonify({'state':1,'message':'Empty name'})))
     if not message:
         message = ''
     else:
@@ -75,13 +87,9 @@ def api_send_email_sync():
     message += 'From '+name
     if replyemail:
         message += '('+replyemail+')'
-    send_email_sync(title='祝你生日快乐！',sender='admin@forwarder.com',receivers=['ztx97@qq.com'],content=message)
-    response = make_response(jsonify({'state':0,'message':'sent'}))
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'x-requested-with,Content-Type'
-    response.headers['Access-Control-Max-Age'] = '3600';
-    return response
+    send_email_sync(title='祝你生日快乐！',sender='admin@forwarder.com',receivers=RECEIVERS,content=message)
+    return allow_cross_response(make_response(jsonify({'state':0,'message':'sent'})))
+
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',port='5050',debug=True)
+    app.run(host=HOST,port=PORT,debug=DEBUG)
